a159a5785b3fefb1b40ef08f096e65e1950cbfd5
heocon1207bee <duckhanh.hoang.1481@gmail.com>
2022-07-29 18:24:45 +0700

    add homework chapter 7

--- H-01/app.js
+++ H-01/app.js
@@ -1,25 +1,46 @@
-const express = require('express');
-const app = express();
-const ejs = require('ejs');
-const path = require('path');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose')
 
-const homeRoute = require('./routes/index');
-const aboutRoute = require('./routes/about');
-const contactRoute = require('./routes/contact');
-const postRoute = require('./routes/post');
+// connect to mongodb
+const database = 'mongodb://localhost/users-homework7'
+mongoose.connect(database)
 
-//use third party middleware - view engine setup
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
-// using middleware - register public folder
-app.use(express.static('public'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', homeRoute);
-app.use('/about', aboutRoute);
-app.use('/contact', contactRoute);
-app.use('/post', postRoute);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.listen(4000, function () {
-    console.log('Express server listening on port 4000');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
